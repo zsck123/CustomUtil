59cd3dc2144d1269c0153b9ce6a36718f638a176
zsck <825352674@qq.com>
2023-01-16 19:46:39 +0800

    优化代码

--- src/main/java/per/zsck/custom/util/BeanUtil.java
+++ src/main/java/per/zsck/custom/util/BeanUtil.java
@@ -4,14 +4,12 @@ import org.springframework.beans.BeanUtils;
 
 /**
  * @author zsck
- * @date 2023/1/15 - 11:35
+ * @date 2023/1/14 - 15:36
  */
-@SuppressWarnings("unused")
 public class BeanUtil {
 
     public static <T> T copyProperties(Object source, T target) {
         BeanUtils.copyProperties(source, target);
         return target;
     }
-
 }
--- src/main/java/per/zsck/custom/util/StreamUtil.java
+++ src/main/java/per/zsck/custom/util/StreamUtil.java
@@ -1,7 +1,10 @@
 package per.zsck.custom.util;
 
 
+import cn.hutool.core.collection.CollectionUtil;
+
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -10,11 +13,11 @@ import java.util.stream.Collectors;
  * @author zsck
  * @date 2023/1/14 - 13:27
  */
-@SuppressWarnings("unused")
 public class StreamUtil {
-
     public static <T, R> Set<R> getSetOf(Collection<T> collection, Function<T, R> function) {
+        if (CollectionUtil.isEmpty(collection)) {
+            return Collections.emptySet();
+        }
         return collection.stream().map(function).collect(Collectors.toSet());
     }
-
 }
